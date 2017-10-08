3236d64f9f2d448fc98c3486ba12720a80de4331
Bill Peterson <william.o.peterson@gmail.com>
2017-10-08 15:38:40 -0700

    Attempt to resolve prod errors

--- server/config/gmaps.js
+++ server/config/gmaps.js
@@ -4,7 +4,6 @@ module.exports = {
 
 	// Call the Google Maps giving it a longitude and latitude and returning an object with address information.
 	getAddy: function(req, res){
-		console.log("Getting address")
 		request("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + req.body.lat + "," + req.body.lng + "&key=AIzaSyDsSkmm5QzQsR9ET_K2oH-BfKo1BoQvSUY", function(err, response, body){
 			if(err){
 				console.log("something went wrong");
@@ -18,17 +17,10 @@ module.exports = {
 		}
 	)},
 
-	// Call the Google Geocoding API giving it search criteria, which is returns latitude and longitude.
+	// Call the Google Geocoding API giving it search criteria, which is returned and latitude and longitude.
 	search: function(req, res){
 		console.log("geocoding:", req.body);
 		request("https://maps.googleapis.com/maps/api/geocode/json?address=" + req.body.search + "&key=AIzaSyDsSkmm5QzQsR9ET_K2oH-BfKo1BoQvSUY", function(err, response, body){
-			body=JSON.parse(body)
-			console.log("---err---")
-			console.log(err);
-			// console.log("---response---")
-			// console.log(response);
-			console.log("---body---")
-			console.log(body.status);
 			if(err){
 				console.log("something went wrong");
 				console.log(err);
@@ -36,8 +28,7 @@ module.exports = {
 			}
 			else {
 				console.log("we got it!");
-				// console.log(body);
-				res.json(body);
+				res.json(JSON.parse(body));
 			}
 		})
 	}
